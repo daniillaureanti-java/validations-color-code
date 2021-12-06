0ee8e83d6a2a631c33f2bbf976195df25c08e3b0
Daniil Laureanti <daniil.laureanti@gmail.com>
2021-12-06 11:08:21 +0200

    Completed task

--- src/main/java/com/epam/rd/autotasks/validations/ColorCodeValidation.java
+++ src/main/java/com/epam/rd/autotasks/validations/ColorCodeValidation.java
@@ -5,8 +5,13 @@ import java.util.regex.Pattern;
 
 public class ColorCodeValidation {
     public static boolean validateColorCode(String color) {
-
-        // Put your code here
+        String regex = "^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$";
+        Pattern pattern = Pattern.compile(regex);
+        if(color==null||color.isEmpty()||color.trim().isEmpty())return false;
+        Matcher matcher =pattern.matcher(color);
+        if(matcher.matches()){
+            return true;
+        }else return false;
 
     }
 }
